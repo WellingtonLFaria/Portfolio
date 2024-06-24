48ac6a7f27d740ced8d5d28c4872b337c4e6f9d3
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-06-24 09:13:23 -0300

    feat: Add more info about each project

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -3,8 +3,10 @@ interface ProjectData {
     anoSemestre: string,
     academicPartner: string,
     description: string,
+    personalContributions: string,
     technologies: string[],
     softSkills: string[],
+    whatCanImprove: string[],
     link: string
 }
 
@@ -15,7 +17,7 @@ type Props = {
 export default function Project({project}: Props) {
     return (
         <div className="project text-white font-jetbrains flex flex-col gap-5">
-            <h4 className="text-3xl 2xl:text-5xl"><a href={project.link} className="text-indigo-500 hover:underline">{project.name} - {project.anoSemestre}</a></h4>
+            <h4 className="text-3xl 2xl:text-5xl"><a href={project.link} target="_blank" className="text-indigo-500 hover:underline">{project.name} - {project.anoSemestre}</a></h4>
             <div>
             <h5 className="text-2xl 2xl:text-4xl mb-2">Academic partner:</h5>
             <p className="font-roboto text-xl 2xl:text-3xl">{project.academicPartner}</p>
@@ -23,6 +25,10 @@ export default function Project({project}: Props) {
             <div>
                 <h5 className="text-2xl 2xl:text-4xl mb-2">Project's goal:</h5>
                 <p className="font-roboto text-xl 2xl:text-3xl">{project.description}</p>
+            </div>
+            <div>
+                <h5 className="text-2xl 2xl:text-4xl mb-2">Personal contributions:</h5>
+                <p className="font-roboto text-xl 2xl:text-3xl">{project.personalContributions}</p>
             </div>
             <div>
                 <h5 className="text-2xl 2xl:text-4xl mb-2">Technologies:</h5>
@@ -37,6 +43,14 @@ export default function Project({project}: Props) {
                 <ul className="text-xl 2xl:text-3xl list-disc list-inside">
                     {project.softSkills.map(softSkill =>
                         <li>{softSkill}</li>
+                    )}
+                </ul>
+            </div>
+            <div>
+                <h5 className="text-2xl 2xl:text-4xl mb-2">What can be improved for the next project:</h5>
+                <ul className="text-xl 2xl:text-3xl list-disc list-inside">
+                    {project.whatCanImprove.map(whatCanImprov =>
+                        <li>{whatCanImprov}</li>
                     )}
                 </ul>
             </div>
--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -3,8 +3,10 @@ interface Project {
     anoSemestre: string,
     academicPartner: string,
     description: string,
+    personalContributions: string,
     technologies: string[],
     softSkills: string[],
+    whatCanImprove: string[],
     link: string
 }
 
@@ -14,6 +16,7 @@ export const projects: Project[] = [
         anoSemestre: "2023-1",
         academicPartner: "Vanguarda",
         description: 'For Rede Vanguarda journalists who wish to access, view and analyze COVID Longa data, "Data-SARS" is a website that allows easy access to information related to COVID Longa. Unlike some sites that propagate Fake News and that do not have search filters, our product provides data in a way that is easy to understand and analyze, since our data is authentic and we have a minimalist interaction area.',
+        personalContributions: "In this project I developed most of the Back-end which was made in Flask, I was responsible for the entire data scraping process which was carried out using the Selenium library, I was also responsible for the graphs made using ChartJS.",
         technologies: [
             "GIT",
             "Python",
@@ -25,9 +28,14 @@ export const projects: Project[] = [
             "JavaScript"
         ],
         softSkills: [
-            "Comunication",
+            "Communication",
             "Collaboration",
             "Team work"
+        ],
+        whatCanImprove: [
+            "Planning",
+            "Knowledge of the technologies used in the project",
+            "Communication"
         ],
         link: "https://github.com/Grupo-Syntax-Squad/API"
     },
@@ -36,6 +44,7 @@ export const projects: Project[] = [
         anoSemestre: "2023-2",
         academicPartner: "FATEC",
         description: 'For companies looking to implement solutions for managing service calls, "CallGenie" is a CRUD system that allows better management of calls, with a minimalist interaction area and documents that help the user to use the system. In this project, we implemented the system for a fictitious computer store, however, our system can be implemented in any area, as long as there is difficulty in managing calls.',
+        personalContributions: "In this project I developed much of the Back-end using Express and helped integrate the Front-end with the Back-end.",
         technologies: [
             "GIT",
             "HTML",
@@ -50,6 +59,10 @@ export const projects: Project[] = [
             "Adaptability",
             "Ability to solve problems"
         ],
+        whatCanImprove: [
+            "Planning",
+            "Communication"
+        ],
         link: "https://github.com/Grupo-Syntax-Squad/CallGenie"
     },
     {
@@ -57,6 +70,7 @@ export const projects: Project[] = [
         anoSemestre: "2024-1",
         academicPartner: "Youtan",
         description: "The project aims to solve problems in asset management control of any companies that use the system. In this sense, the application must contain interfaces for monitoring, registration and control over assets, also providing options for their delegation and manipulation. Above all, it is also crucial to generate indicator reports, issue notifications regarding the status of the asset and register recipient users for the assets.",
+        personalContributions: "In this project I developed a large part of the Back-end made in Java with SpringBoot, I also carried out a large part of the integration of the Front-end with the Back-end, I also developed some Front-end pages.",
         technologies: [
             "GIT",
             "HTML",
@@ -70,6 +84,10 @@ export const projects: Project[] = [
         softSkills: [
             "Planning",
             "Work under pressure"
+        ],
+        whatCanImprove: [
+            "Planning",
+            "Time management"
         ],
         link: "https://github.com/Grupo-Syntax-Squad/AssetBox"
     }
