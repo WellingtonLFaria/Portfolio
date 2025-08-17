e804b57318aa310cff9c1a0f99121a628bf7bee9
WellingtonLFaria <wellingtonll.faria@gmail.com>
2025-08-17 17:42:59 -0300

    feature: translate portfolio to portuguese and updated content

--- _next/static/chunks/main-285d60aabf792496.js
+++ _next/static/chunks/main-285d60aabf792496.js
@@ -531,7 +531,7 @@
                             640, 750, 828, 1080, 1200, 1920, 2048, 3840,
                           ],
                           imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-                          path: "/Portfolio/_next/image/",
+                          path: "/Portfolio/Portfolio/_next/image/",
                           loader: "default",
                           dangerouslyAllowSVG: !1,
                           unoptimized: !1,
