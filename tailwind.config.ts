c8b343ba6de58f10bd0df4674e4ee96e840d1ad6
WellingtonLFaria <wellingtonll.faria@gmail.com>
2025-08-17 02:54:26 -0300

    feature: added /out to tailwind settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./out/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./out/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./out/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        "jetbrains": ["var(--font-jetbrains)"],
-        "roboto": ["var(--font-roboto)"]
-      }
+        jetbrains: ["var(--font-jetbrains)"],
+        roboto: ["var(--font-roboto)"],
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
